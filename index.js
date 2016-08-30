fb6558eb0d4761808c7da0d107eabbbdd5227eec
litingjun2015 <tingjun.li@outlook.com>
2016-08-30 09:39:04 +0800

    add basic

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = function(app){
+　　app.get('/',function(req,res){
+       res.render('test',{issuccess:"success"})
+    }); 
+    app.get('/interface',function(req,res){});   
+    app.post('/interface',function(req,res){});  
+}
